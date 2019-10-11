0f1311bde9c93e15838dfbf9391563da7b75b776
itrebligjl <jgilberti@archemy.com>
2019-10-11 19:32:16 -0400

    Adding changes to GitTest.java and Readme.md

--- GitSample/src/com/git/sample/GitTest.java
+++ GitSample/src/com/git/sample/GitTest.java
@@ -3,5 +3,6 @@ package com.git.sample;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Testing the Git Repository Operations!");
+		System.out.println("Adding more comments to add change to be committed to the Git Reporsitory!");
 	}
 }
